14e0b55104c4ed9284a49bd6125a172f658a94b8
ArianeMB <arianebarboza2003@hotmail.com>
2023-02-10 21:54:41 -0300

    Primeiro Commit

---
+++ ExercicioEmAula01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+//Exercício feito dia 10/02/2023)
+public class ExercicioEmAula01 {
+
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+
+
+
+    }
+}
